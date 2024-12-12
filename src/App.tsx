af11bbce10c744dd15a8ebd66ec970976c21201e
Mikhail <bolotnikovms@yandex.ru>
2024-12-12 20:56:53 +0500

    feat: Добавлена кнопка

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
+import { Calculator } from 'lucide-react'
 import { useState } from 'react'
 import './App.css'
+import { Button } from './components/ui/button'
 import { Input } from './components/ui/input'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './components/ui/select'
 
+const voltageClassess = [
+  {value: '6000', lable: '6'},
+  {value: '10000', lable: '10'},
+  {value: '35000', lable: '35'},
+  {value: '110000', lable: '110'},
+  {value: '220000', lable: '220'},
+  {value: '500000', lable: '500'},
+]
+
 function App() {
-  const [count, setCount] = useState<number>(0)
+  const [result, setResult] = useState<number>(0)
+  const [voltage, setVoltage] = useState<number | null>(null)
+
+  const changeVoltage = (value: string) => setVoltage(+value)
+  const calculation = () => {
+    console.log('clik')
+  }
 
   return (
     <div className='flex flex-col justify-center gap-4'>
       <div className='flex flex-wrap items-center justify-center gap-4'>
         <Input className='max-w-[250px]' aria-autocomplete='none' />
-        <Select>
-          <SelectTrigger className='max-w-[250px]'>
-            <SelectValue placeholder="Выберите напряжение" />
+        <Select onValueChange={changeVoltage}>
+          <SelectTrigger className='max-w-[250px]' >
+            <SelectValue placeholder="Выберите напряжение"/>
           </SelectTrigger>
           <SelectContent>
-            <SelectItem value="6">6</SelectItem>
-            <SelectItem value="10">10</SelectItem>
-            <SelectItem value="35">35</SelectItem>
-            <SelectItem value="110">110</SelectItem>
-            <SelectItem value="220">220</SelectItem>
-            <SelectItem value="500">500</SelectItem>
+            {voltageClassess.map(voltage => (
+              <SelectItem key={voltage.value} value={voltage.value}>{voltage.lable}</SelectItem>
+            ))}
           </SelectContent>
         </Select>
         <Select>
@@ -32,9 +46,12 @@ function App() {
             <SelectItem value="10">Мощность</SelectItem>
           </SelectContent>
         </Select>
+        <Button variant="outline" size="icon" className='hover:bg-gray-300/85 transition ease-out duration-500'  onClick={calculation}>
+          <Calculator />
+        </Button>
       </div>
       <div>
-        <p className='text-white'>Результат: <span>10</span></p>
+        <p className='text-white'>Результат: <span>{result}</span></p>
       </div>
     </div>
   )
