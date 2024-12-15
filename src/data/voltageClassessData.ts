0b0adf3242f7a57f51e95776f31fdc45ed4d6276
Mikhail <bolotnikovms@yandex.ru>
2024-12-15 17:34:55 +0500

    refactor: Данные вынесены

---
+++ src/data/voltageClassessData.ts
@@ -0,0 +1,8 @@
+export const voltageClassessData = [
+  { value: '6', lable: '6' },
+  { value: '10', lable: '10' },
+  { value: '35', lable: '35' },
+  { value: '110', lable: '110' },
+  { value: '220', lable: '220' },
+  { value: '500', lable: '500' },
+]
