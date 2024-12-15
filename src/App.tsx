abf30a09edb6da8fd7b7b315692c217ee53d55c7
Mikhail <bolotnikovms@yandex.ru>
2024-12-15 17:35:22 +0500

    refactor: Рефакторинг

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,118 @@
 import { Calculator } from 'lucide-react'
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import './App.css'
+import Error from './components/ordinary/error'
+import InformationQuanta from './components/simple/informationQuanta'
+import Results from './components/simple/results'
 import { Button } from './components/ui/button'
 import { Input } from './components/ui/input'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './components/ui/select'
+import { typeCalculationData } from './data/typeCalculationData'
+import { voltageClassessData } from './data/voltageClassessData'
+import { IError } from './interfaces/error.interface'
 
-const voltageClassess = [
-  {value: '6000', lable: '6'},
-  {value: '10000', lable: '10'},
-  {value: '35000', lable: '35'},
-  {value: '110000', lable: '110'},
-  {value: '220000', lable: '220'},
-  {value: '500000', lable: '500'},
-]
+const App = () => {
+  const [currentCoefficient, setCurrentCoefficient] = useState<string>('')
+  const [quanta, setQuanta] = useState<string>('')
+  const [voltage, setVoltage] = useState<number>(6)
+  const [typeCalculation, setTypeCalculation] = useState<string>('amperage')
+  const [coefficient, setCoefficient] = useState<number>(0)
+  const [bias, setBias] = useState<number>(0)
+  const [errors, setErrors] = useState<IError>({ errorCurrentCoefficient: '', errorQuanta: '' })
 
-function App() {
-  const [result, setResult] = useState<number>(0)
-  const [voltage, setVoltage] = useState<number | null>(null)
+  const changeCurrentCoefficient = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
 
+    if (!Number(value)) {
+      setCurrentCoefficient('')
+      setErrors({ ...errors, errorCurrentCoefficient: 'Поле "Коэффициент по току" должно быть положительным числом!' })
+
+      return
+    }
+
+    setErrors({ ...errors, errorCurrentCoefficient: '' })
+    return setCurrentCoefficient(value)
+  }
+  const changeQuanta = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+
+    if (!Number(value)) {
+      setQuanta('')
+      setErrors({ ...errors, errorQuanta: 'Поле "Кванты" должно быть положительным числом!' })
+
+      return
+    }
+
+    setErrors({ ...errors, errorQuanta: '' })
+    return setQuanta(value)
+  }
   const changeVoltage = (value: string) => setVoltage(+value)
+  const changeTypeCalculation = (value: string) => setTypeCalculation(value)
   const calculation = () => {
-    console.log('clik')
+    setBias(0)
+
+    if (!currentCoefficient || !quanta) return
+
+    switch (typeCalculation) {
+      case 'amperage':
+        setCoefficient(+currentCoefficient / +quanta)
+
+        break
+      case 'voltage1':
+        setCoefficient(voltage * 1.25 / +quanta)
+
+        break
+      case 'voltage2':
+        setCoefficient((voltage * 1.25) / (+quanta * 2.5))
+        setBias(voltage * 0.75)
+
+        break
+      case 'power':
+        setCoefficient((+currentCoefficient * voltage * Math.sqrt(3)) / +quanta)
+
+        break
+      case 'powerReverse':
+        setCoefficient(((+currentCoefficient * voltage * Math.sqrt(3)) / +quanta) * 2)
+        setBias(-(+currentCoefficient * voltage * Math.sqrt(3)))
+
+        break
+      default:
+        break
+    }
   }
 
   return (
-    <div className='flex flex-col justify-center gap-4'>
-      <div className='flex flex-wrap items-center justify-center gap-4'>
-        <Input className='max-w-[250px]' aria-autocomplete='none' />
-        <Select onValueChange={changeVoltage}>
-          <SelectTrigger className='max-w-[250px]' >
-            <SelectValue placeholder="Выберите напряжение"/>
-          </SelectTrigger>
-          <SelectContent>
-            {voltageClassess.map(voltage => (
-              <SelectItem key={voltage.value} value={voltage.value}>{voltage.lable}</SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-        <Select>
-          <SelectTrigger className='max-w-[250px]'>
-            <SelectValue placeholder="Выберите тип" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="6">Ток</SelectItem>
-            <SelectItem value="10">Мощность</SelectItem>
-          </SelectContent>
-        </Select>
-        <Button variant="outline" size="icon" className='hover:bg-gray-300/85 transition ease-out duration-500'  onClick={calculation}>
-          <Calculator />
-        </Button>
+    <div className='flex flex-wrap justify-center gap-6'>
+      <div className='flex flex-col gap-4'>
+        <div className='flex flex-col items-center gap-4'>
+          <Input className='max-w-[300px]' onChange={(e) => changeCurrentCoefficient(e)} value={currentCoefficient} placeholder='Коэффициент по току' aria-autocomplete='none' />
+          <Input className='max-w-[300px]' onChange={(e) => changeQuanta(e)} value={quanta} placeholder='Кванты' aria-autocomplete='none' />
+          <Select onValueChange={changeVoltage} defaultValue={voltage.toString()}>
+            <SelectTrigger className='max-w-[300px]' >
+              <SelectValue placeholder="Напряжение" />
+            </SelectTrigger>
+            <SelectContent>
+              {voltageClassessData.map(voltage => (
+                <SelectItem key={voltage.value} value={voltage.value}>{voltage.lable}</SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <Select onValueChange={changeTypeCalculation} defaultValue={typeCalculation}>
+            <SelectTrigger className='max-w-[300px]'>
+              <SelectValue placeholder="Тип расчета" />
+            </SelectTrigger>
+            <SelectContent>
+              {typeCalculationData.map(item => (<SelectItem key={item.value} value={item.value}>{item.lable}</SelectItem>))}
+            </SelectContent>
+          </Select>
+          <Button variant="outline" size="icon" className='w-[300px] hover:bg-gray-300/85 transition ease-out duration-500' onClick={calculation}>
+            <Calculator />
+          </Button>
+        </div>
+        <Results coefficient={coefficient} bias={bias} />
       </div>
-      <div>
-        <p className='text-white'>Результат: <span>{result}</span></p>
-      </div>
+      <InformationQuanta />
+      <Error errorCurrentCoefficient={errors.errorCurrentCoefficient} errorQuanta={errors.errorQuanta} />
     </div>
   )
 }
