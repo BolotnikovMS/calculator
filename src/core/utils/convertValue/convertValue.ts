ff83b9e8f9cde7b4b8382c2ed5fe06f4249f387f
Mikhail <bolotnikovms@yandex.ru>
2024-12-18 18:46:44 +0500

    feat: Фун-я перевода значения

---
+++ src/core/utils/convertValue/convertValue.ts
@@ -0,0 +1,16 @@
+import { TConvertValue } from "./convertValue.type"
+
+export const convertValue: TConvertValue = (value, quantity) => {
+  switch (quantity) {
+    case 'watts':
+      return value
+    case 'kilowatts':
+      return value / 1000
+    case 'megawatts':
+      return value / 1000 / 1000
+    default:
+      console.error('Ошибка при переводе значения!')
+
+      return 0
+  }
+}
