e1cdba853517cdc4e9278ea47ad361885c3f4dee
Mikhail <bolotnikovms@yandex.ru>
2024-12-24 21:01:44 +0500

    refactor: Переработан компонент
    Получение ошибок из стора

--- src/components/ordinary/error/index.tsx
+++ src/components/ordinary/error/index.tsx
@@ -1,18 +1,21 @@
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import { useErrorStore } from '@/core/store/errors'
 import { AlertCircle } from 'lucide-react'
 import { type FC } from 'react'
-import { IPropsError } from './error.interface'
 
-const Error: FC<IPropsError> = ({ errorCurrentCoefficient, errorQuanta }) => {
-  if (!errorCurrentCoefficient && !errorQuanta) return null
+const Error: FC = () => {
+  const errors = useErrorStore(state => state.errors)
+
+  if (!errors.length) return null
 
   return (
     <Alert variant="destructive">
       <AlertCircle className="h-4 w-4" />
       <AlertTitle>Ошибка ввода!</AlertTitle>
       <AlertDescription>
-        {errorCurrentCoefficient && (<p>{errorCurrentCoefficient}</p>)}
-        {errorQuanta && (<p>{errorQuanta}</p>)}
+        {errors.map(error => (
+          <p key={error.id}>{error.message}</p>
+        ))}
       </AlertDescription>
     </Alert>
   )
