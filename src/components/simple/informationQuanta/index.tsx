5aab7bb0d0c8738b04e17bbada8d96bd358ebb60
Mikhail <bolotnikovms@yandex.ru>
2024-12-24 21:29:52 +0500

    refactor: Вывод на основе данных

--- src/components/simple/informationQuanta/index.tsx
+++ src/components/simple/informationQuanta/index.tsx
@@ -1,66 +1,30 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { quantaData } from '@/data/quantaData'
 import { type FC } from 'react'
 
 const InformationQuanta: FC = () => {
   return (
     <Tabs defaultValue="aris" className="w-[400px]">
       <TabsList>
-        <TabsTrigger value="aris">Aris</TabsTrigger>
-        <TabsTrigger value="iset">Исеть</TabsTrigger>
-        <TabsTrigger value="granit">Гранит</TabsTrigger>
-        <TabsTrigger value="telecontrol">Телеконтроль</TabsTrigger>
+        {quantaData.map(item => (
+          <TabsTrigger value={item.device}>{item.name}</TabsTrigger>
+        ))}
       </TabsList>
-      <TabsContent value="aris">
-        <Card>
-          <CardHeader className='p-3'>
-            <CardTitle>Кванты Aris</CardTitle>
-          </CardHeader>
-          <CardContent className="p-3">
-            <div>
-              <p>Инфа арис</p>
-            </div>
-          </CardContent>
-        </Card>
-      </TabsContent>
-      <TabsContent value="iset">
-        <Card>
-          <CardHeader className='p-3'>
-            <CardTitle>Кванты Исети</CardTitle>
-          </CardHeader>
-          <CardContent className="p-3">
-            <div>
-              <p>Инфа Исеть</p>
-            </div>
-          </CardContent>
-        </Card>
-      </TabsContent>
-      <TabsContent value="granit">
-        <Card>
-          <CardHeader className='p-3'>
-            <CardTitle>Кванты Гранита</CardTitle>
-          </CardHeader>
-          <CardContent className="p-3">
-            <div>
-              <p>Инфа Гранит. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur enim unde repellendus. Iusto, non exercitationem voluptas nobis aperiam, culpa earum fugiat quo laboriosam delectus voluptates nemo error pariatur molestias! Quis.
-              </p>
-            </div>
-          </CardContent>
-        </Card>
-      </TabsContent>
-      <TabsContent value="telecontrol">
-        <Card>
-          <CardHeader className='p-3'>
-            <CardTitle>Кванты Телеконтроля</CardTitle>
-          </CardHeader>
-          <CardContent className="p-3">
-            <div>
-              <p>Инфа Гранит. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur enim unde repellendus. Iusto, non exercitationem voluptas nobis aperiam, culpa earum fugiat quo laboriosam delectus voluptates nemo error pariatur molestias! Quis.
-              </p>
-            </div>
-          </CardContent>
-        </Card>
-      </TabsContent>
+      {quantaData.map(item => (
+        <TabsContent value={item.device}>
+          <Card>
+            <CardHeader className='p-3'>
+              <CardTitle>Информаци о {item.name}</CardTitle>
+            </CardHeader>
+            <CardContent className="p-3">
+              <div>
+                <p>{item.description}</p>
+              </div>
+            </CardContent>
+          </Card>
+        </TabsContent>
+      ))}
     </Tabs>
   )
 }
